13861358328dea201874e7b12ebc6870990e1203
Dominic Serrano <dmsrojo@gmail.com>
2016-10-25 15:37:20 -0500

    Server now takes picture and streams it to main server after 6 second timeout. File too big for mongodb

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const express = require('express');
+const app = express();
 const { createReadStream } = require('fs');
 const request = require('request');
+const exec = require('child_process').exec;
 /////////////////////////////////////////
 
-const imageStream = createReadStream('dice.png');
+let counter = 1;
 
-imageStream
-.pipe(request.post('http://localhost:3000/api/image/new'));
+const takePicture = () => {
+  return new Promise((resolve, reject) => {
+    exec(`raspistill -o ~/spy-on-fido/image1.jpg`);
+    counter++;
+    resolve();
+  })
+};
+
+takePicture()
+.then(() => {
+  setTimeout(() => {
+  console.log('Read stream now')
+  createReadStream('image1.jpg')
+  .pipe(request.post('http://10.0.0.168:3000/api/image/new'));
+  }, 6000);
+})
+.catch((err) => {
+  console.log(err);
+});
+
+
+app.listen(3000, () => console.log(`Currently listening on port 3000`));
