780833b7e70dfb1957c178893a7f132e958adf43
Dominic Serrano <dmsrojo@gmail.com>
2016-11-11 09:24:46 -0600

    Final edit for readme. Converted to original repo, need to update the js files with the files from RPI

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ const express = require('express');
 const app = express();
 const takeImage = require('./commands/takeImage.js');
 const takeVideo = require('./commands/takeVideo.js');
+const pb_takeImage = require('./commands/pb_takeImage.js');
+const pb_takeVideo = require('./commands/pb_takeVideo.js');
 const { red, cyan } = require('chalk');
+const { readdirSync } = require('fs');
 /////////////////////////////////////////
 
-// takeImage();
-// takeVideo();
+
+console.log("Test test", readdirSync('photoBooth/images').slice(-1));
+
+app.set('view engine', 'pug');
+app.use(express.static('photoBooth'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.post('/rpi/image/single', takeImage);
+app.post('/rpi/video/static', takeVideo);
+app.get('/pb/image', pb_takeImage);
+app.get('/pb/video', pb_takeVideo);
+
 
 app.use(( err, { url }, res, next) => {
   const timeStamp = Date().slice(16, -15);
@@ -16,7 +33,7 @@ app.use(( err, { url }, res, next) => {
     `${red(timeStamp)} [${red(`${url}`)}]`
   );
   console.error(err.stack);
-})
+});
 
 
 app.listen(3000, () => {
